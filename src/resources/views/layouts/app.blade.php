65b8743a0b9f4be1195ce2909e67ca9eefe30f1e
有栖 <sekka.alice@gmail.com>
2023-07-17 19:52:44 +0900

    [update] layoutsのデザインの調整。

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -14,23 +14,65 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
+
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <div class="w-screen h-screen bg-white">
+            <div class="related w-[1200px] h-full mx-auto">
+                <!-- Page Header -->
+                <div class="flex justify-between items-center px-6 pt-6 pb-3">
+                    <header >
                         {{ $header }}
-                    </div>
-                </header>
-            @endif
+                    </header>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+                    <div class="absolute right-0 flex items-center justify-end p-3 pr-36 bg-gray-100">
+                        <!-- 検索 -->
+                        <form action="{{ route('daily.index') }}" method="POST" class="w-full mr-96">
+                            @csrf
+                            <input type="text" />
+                            <x-primary-button type="submit" class="ml-5">Search</x-primary-button>
+                        </form>
+
+                        <!-- ログアウトボタン -->
+                        <div class="flex items-center ml-6">
+                            <x-dropdown align="right" width="48">
+                                <x-slot name="trigger">
+                                    <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                        <div>{{ Auth::user()->name }}</div>
+
+                                        <div class="ml-1">
+                                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                            </svg>
+                                        </div>
+                                    </button>
+                                </x-slot>
+
+                                <x-slot name="content">
+                                    <x-dropdown-link :href="route('profile.edit')">
+                                        {{ __('Profile') }}
+                                    </x-dropdown-link>
+
+                                    <!-- Authentication -->
+                                    <form method="POST" action="{{ route('logout') }}">
+                                        @csrf
+
+                                        <x-dropdown-link :href="route('logout')"
+                                                onclick="event.preventDefault();
+                                                            this.closest('form').submit();">
+                                            {{ __('Log Out') }}
+                                        </x-dropdown-link>
+                                    </form>
+                                </x-slot>
+                            </x-dropdown>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Page Content -->
+                <main class="px-6 pt-3 pb-6">
+                    {{ $slot }}
+                </main>
+            </div>
         </div>
     </body>
 </html>
