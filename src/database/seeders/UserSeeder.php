e4936b04730e859c21536a89da778c7070a6f101
有栖 <sekka.alice@gmail.com>
2023-07-17 14:18:35 +0900

    [add] usersのシーディングを作成。

---
+++ src/database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i = 1; $i <= 10; $i++){
+            DB::table('users')->insert([
+                'name' => 'sample'. $i,
+                'email' => 'sample'. $i .'@gmail.com',
+                'password' => Hash::make('sample'. $i),
+            ]);
+        }
+    }
+}
