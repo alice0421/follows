17088009d89c809403abfca3d601b3d64ebec5ba
有栖 <sekka.alice@gmail.com>
2023-07-19 01:39:08 +0900

    [update] デザインの修正。

--- src/resources/views/dailies/index.blade.php
+++ src/resources/views/dailies/index.blade.php
@@ -6,16 +6,25 @@
     </x-slot>
 
     <div>
-        @foreach ($dailies as $daily)
-            <div class="pb-10">
-                <a href="{{ route('daily.show', ['daily' => $daily->id]) }}"
-                class="inline text-2xl text-blue-500 font-bold underline hover:no-underline"
-                >
-                    {{ $daily->title }}
-                </a>
-                <span class="pl-10 text-sm">{{ $daily->user->name }}</span>
-                <p class="pt-3 text-base">{{ $daily->body }}</p>
-            </div>
-        @endforeach
+        <a href="{{ route('daily.create') }}">
+            <x-primary-button type="button">新規作成</x-primary-button>
+        </a>
+
+        <div class="pt-5">
+            @foreach ($dailies as $daily)
+                <div class="pb-10">
+                    <a href="{{ route('daily.show', ['daily' => $daily->id]) }}"
+                    class="block w-60 truncate text-2xl text-blue-500 font-bold underline hover:no-underline"
+                    >
+                        {{ $daily->title }}
+                    </a>
+                    <div class="flex pt-1 item-center text-gray-400 text-sm">
+                        <p>{{ $daily->date }}</p>
+                        <p class="pl-5">{{ $daily->user->name }}</p>
+                    </div>
+                    <p class="pt-3 text-base w-80 h-10 truncate">{{ $daily->body }}</p>
+                </div>
+            @endforeach
+        </div>
     </div>
 </x-app-layout>
