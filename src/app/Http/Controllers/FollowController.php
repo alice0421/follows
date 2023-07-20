2084848d0904d6a03b8d5eb4fa6e4f03ac0d1abe
有栖 <sekka.alice@gmail.com>
2023-07-20 19:39:28 +0900

    [update] フレンド登録機能・一覧機能をプロフィール画面に移植。

--- src/app/Http/Controllers/FollowController.php
+++ src/app/Http/Controllers/FollowController.php
@@ -9,12 +9,12 @@ use Illuminate\Support\Facades\Auth;
 
 class FollowController extends Controller
 {
-    public function index()
-    {
-        // $followers = Auth::user()->followers()->get(); // フォロワー
-        $followings = Auth::user()->followings()->get(); // 自分がフォローしている人
-        return view('follows.index')->with(['followings' => $followings]);
-    }
+    // public function index()
+    // {
+    //     $followers = Auth::user()->followers()->get(); // フォロワー
+    //     $followings = Auth::user()->followings()->get(); // 自分がフォローしている人
+    //     return view('follows.index')->with(['followings' => $followings]);
+    // }
 
     public function register(Request $request)
     {
--- src/app/Http/Controllers/ProfileController.php
+++ src/app/Http/Controllers/ProfileController.php
@@ -16,8 +16,10 @@ class ProfileController extends Controller
      */
     public function edit(Request $request): View
     {
+        $followings = Auth::user()->followings()->get(); // 自分がフォローしている人
         return view('profile.edit', [
             'user' => $request->user(),
+            'followings' => $followings,
         ]);
     }
 
