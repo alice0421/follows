cbd37d1465bdb9ed3bb78ae908c51fea433b4726
有栖 <sekka.alice@gmail.com>
2023-07-20 20:58:38 +0900

    [update] 枠線の追加。

--- src/resources/views/profile/edit.blade.php
+++ src/resources/views/profile/edit.blade.php
@@ -7,19 +7,19 @@
 
     <div class="flex w-full">
         <div class="w-1/2">
-            <div class="p-8 bg-white shadow rounded-md">
+            <div class="p-8 border border-gray-300 bg-white shadow rounded-md">
                 <div class="w-4/5">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="mt-6 p-8 bg-white shadow rounded-md">
+            <div class="mt-6 p-8 border border-gray-300 bg-white shadow rounded-md">
                 <div class="w-4/5">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="mt-6 p-8 bg-white shadow rounded-md">
+            <div class="mt-6 p-8 border border-gray-300 bg-white shadow rounded-md">
                 <div class="w-4/5">
                     @include('profile.partials.delete-user-form')
                 </div>
@@ -27,7 +27,7 @@
         </div>
 
         <div class="ml-3 w-1/2">
-            <div class="p-8 bg-white shadow rounded-md">
+            <div class="p-8 border border-gray-300 bg-white shadow rounded-md">
                 @include('profile.partials.follow-users')
             </div>
         </div>
