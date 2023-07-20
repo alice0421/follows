e886dc7cf0d0d9a2ccfc1965b9c5b7d58ba2fb47
有栖 <sekka.alice@gmail.com>
2023-07-21 02:56:03 +0900

    [update] 日記詳細画面のデザイン微調整。

--- src/resources/views/dailies/show.blade.php
+++ src/resources/views/dailies/show.blade.php
@@ -5,22 +5,14 @@
         </h2>
     </x-slot>
 
-    <div>
-        <a href="{{ route('daily.index') }}"
-        class="inline text-base text-blue-500 font-bold underline hover:no-underline"
-        >
-            <x-primary-button type="button">ホームへ</x-primary-button>
-        </a>
-
-        <div class="w-[600px] py-5 pb-10">
-            <h1 class="text-2xl font-bold">
-                {{ $daily->title }}
-            </h1>
-            <div class="flex pt-1 item-center text-gray-400 text-sm">
-                <p>{{ $daily->date }}</p>
-                <p class="pl-5">{{ $daily->user->name }}</p>
-            </div>
-            <p class="pt-5 text-lg">{{ $daily->body }}</p>
+    <div class="w-[600px] py-5 pb-10">
+        <h1 class="text-2xl font-bold">
+            {{ $daily->title }}
+        </h1>
+        <div class="flex pt-1 item-center text-gray-400 text-sm">
+            <p>{{ $daily->date }}</p>
+            <p class="pl-5">{{ $daily->user->name }}</p>
         </div>
+        <p class="pt-5 text-lg">{{ $daily->body }}</p>
     </div>
 </x-app-layout>
