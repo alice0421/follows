464ff64db1568589dc7534a5fcd8818c441b967d
有栖 <sekka.alice@gmail.com>
2023-07-21 17:36:21 +0900

    [update] フレンドの日記を降順で表示。

--- src/resources/views/dailies/index.blade.php
+++ src/resources/views/dailies/index.blade.php
@@ -5,34 +5,71 @@
         </h2>
     </x-slot>
 
-    @foreach ($dailies as $daily)
-        <div class="pb-10">
-            <a
-                href="{{ route('daily.show', ['daily' => $daily->id]) }}"
-                class="block w-60 truncate text-2xl text-blue-500 font-bold underline hover:no-underline"
-            >
-                {{ $daily->title }}
-            </a>
+    <div class="flex">
+        <!-- 自分の日記 -->
+        <div>
+            @foreach ($my_dailies as $my_daily)
+                <div class="pb-10">
+                    <a
+                        href="{{ route('daily.show', ['daily' => $my_daily->id]) }}"
+                        class="block w-60 truncate text-2xl text-blue-500 font-bold underline hover:no-underline"
+                    >
+                        {{ $my_daily->title }}
+                    </a>
 
-            <div class="flex pt-1 item-center text-gray-400 text-sm">
-                <p>{{ $daily->date }}</p>
-                <p class="pl-5">{{ $daily->user->name }}</p>
-            </div>
-            
-            <p class="pt-3 text-base w-80 h-10 truncate">{{ $daily->body }}</p>
-            <form action="/daily/{{ $daily->id }}" id="form_{{ $daily->id }}" method="post">
-                @csrf
-                @method('DELETE')
-                <button
-                    type="button"
-                    onclick="deleteDaily({{ $daily->id }})"
-                    class="mt-1 px-2 py-1 text-sm bg-red-200 rounded-md hover:bg-red-300"
-                >
-                    削除
-                </button>
-            </form>
+                    <div class="flex pt-1 item-center text-gray-400 text-sm">
+                        <p>{{ $my_daily->date }}</p>
+                        <p class="pl-5">{{ $my_daily->user->name }}</p>
+                    </div>
+                    
+                    <p class="pt-3 text-base w-80 h-10 truncate">{{ $my_daily->body }}</p>
+                    <form action="/daily/{{ $my_daily->id }}" id="my_form_{{ $my_daily->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button
+                            type="button"
+                            onclick="deleteDaily({{ $my_daily->id }})"
+                            class="mt-1 px-2 py-1 text-sm bg-red-200 rounded-md hover:bg-red-300"
+                        >
+                            削除
+                        </button>
+                    </form>
+                </div>
+            @endforeach
         </div>
-    @endforeach
+
+        <!-- フォローしているユーザー (フレンド) の日記 -->
+        <div>
+            @foreach ($following_dailies as $following_daily)
+                <div class="pb-10">
+                    <a
+                        href="{{ route('daily.show', ['daily' => $following_daily->id]) }}"
+                        class="block w-60 truncate text-2xl text-blue-500 font-bold underline hover:no-underline"
+                    >
+                        {{ $following_daily->title }}
+                    </a>
+
+                    <div class="flex pt-1 item-center text-gray-400 text-sm">
+                        <p>{{ $following_daily->date }}</p>
+                        <p class="pl-5">{{ $following_daily->user->name }}</p>
+                    </div>
+                    
+                    <p class="pt-3 text-base w-80 h-10 truncate">{{ $following_daily->body }}</p>
+                    <form action="/daily/{{ $following_daily->id }}" id="following_form_{{ $following_daily->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button
+                            type="button"
+                            onclick="deleteDaily({{ $following_daily->id }})"
+                            class="mt-1 px-2 py-1 text-sm bg-red-200 rounded-md hover:bg-red-300"
+                        >
+                            削除
+                        </button>
+                    </form>
+                </div>
+            @endforeach
+        </div>
+    </div>
 </x-app-layout>
 
 <script>
